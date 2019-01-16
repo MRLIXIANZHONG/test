5833f8c0677d3667ef9d281453f113abfb44e918
mr.l <1967963174>
2019-01-16 15:45:57 +0800

    第二次提交

--- test.go
+++ test.go
@@ -3,15 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	a := []int{1,2,5,6,7}
+	a := []int{1, 2, 5, 6, 7}
 	//b := []int{1,2,5,6}
 	//b :=remove(a,2)
-	a = append(a[:2],a[3:]...)
-	fmt.Println(a)
+	a = append(a[:2], a[3:]...)
+	b := append(a[:2], a[3:]...)
+	fmt.Println(a, b)
 }
 
-func remove(a []int,i int)[]int  {
-	copy(a[i:],a[i+1:])
+func remove(a []int, i int) []int {
+	copy(a[i:], a[i+1:])
 	return a[:len(a)-1]
 
 }
